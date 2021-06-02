8d4384793e947f1a835b0e22e0c9a462ca5fab5d
Artem Ploujnikov <flexthink@users.noreply.github.com>
2021-06-02 08:57:05 -0400

    Add G2P pretraining (work in progress)

--- tests/unittests/test_synthesis.py
+++ tests/unittests/test_synthesis.py
@@ -1,3 +1,4 @@
+from speechbrain.pretrained.interfaces import GraphemeToPhoneme
 import torch
 from speechbrain.pretrained import SpeechSynthesizer
 from speechbrain.utils.data_pipeline import takes, provides
@@ -13,9 +14,15 @@ model_output_keys: ['output']
 """
 
 
-class TestModel(nn.Module):
+class TestSynthesisModel(nn.Module):
     def forward(self, input):
         return input + 1.0
+
+class TestG2PModel(nn.Module):
+    def forward(self, input):
+        return torch.tensor(
+            [[[0.9, 0.05, 0.05], [0.1, 0.2, 0.7], [0.2, 0.6, 0.2]],
+             [[0.1, 0.1, 0.8], [0.1, 0.7, 0.2], [0.7, 0.1, 0.2]]])
 
 
 def test_synthesizer():
@@ -42,7 +49,7 @@ def test_synthesizer():
         return model_output + torch.tensor([1.0, 2.0, 3.0])
 
     test_hparams = {
-        "model": TestModel(),
+        "model": TestSynthesisModel(),
         "encode_pipeline": {
             "batch": False,
             "steps": [encode_text, model_input],
@@ -56,3 +63,49 @@ def test_synthesizer():
     synthesizer = SpeechSynthesizer(hparams=test_hparams)
     output = synthesizer("test")
     assert torch.isclose(output, torch.tensor([3.0, 5.0, 7.0])).all()
+
+def test_g2p():
+    encoder = TextEncoder()
+    encoder.update_from_iterable(ALPHABET)
+
+    phonemes = ["EY", "BEE", "SEE"]
+
+    @takes("txt")
+    @provides("txt_encoded")
+    def encode_text(txt):
+        return encoder.encode_sequence_torch(txt.upper())
+
+    @takes("txt_encoded")
+    @provides("input")
+    def model_input(txt_encoded):
+        assert txt_encoded.size(-1) == 3
+        return torch.tensor([[1.0, 2.0, 3.0]])
+
+    @takes("output")
+    @provides("phonemes")
+    def decode_g2p(model_output):
+        indices = model_output.argmax(dim=-1)
+        return [
+            [phonemes[index] for index in batch] for batch in indices]
+
+    test_hparams = {
+        "model": TestG2PModel(),
+        "phonemes": phonemes,
+        "encode_pipeline": {
+            "batch": False,
+            "steps": [encode_text, model_input],
+            "output_keys": ["input"],
+        },
+        "decode_pipeline": {"steps": [decode_g2p], "batch": True},
+        "model_input_keys": ["input"],
+        "model_output_keys": ["output"],
+    }
+    g2p = GraphemeToPhoneme(hparams=test_hparams)
+    assert g2p.phonemes == ["EY", "BEE", "SEE"]
+    result = g2p(["ACB", "CBA"])
+    ref_phonemes = [
+        ["EY", "SEE", "BEE"],
+        ["SEE", "BEE", "EY"]]
+    assert ref_phonemes == result
+
+
