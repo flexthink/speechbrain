4f9a9fde465647bcb116c2bb82eb075d93727eab
flexthink <flexthink@users.noreply.github.com>
2023-01-18 21:40:07 -0500

    MAdmixture: Initial set-up

---
+++ speechbrain/lobes/models/madmixture/madmixture.py
@@ -0,0 +1,304 @@
+"""
+MadMixture shared latent space multimodal audio model
+
+Authors
+ * Artem Ploujnikov 2022-2023
+"""
+from torch import nn
+from speechbrain.nnet.attention import MultiheadAttention
+from speechbrain.nnet.CNN import Conv1d, ConvTranspose1d
+from speechbrain.dataio.dataio import length_to_mask
+from math import floor, ceil
+
+class MadMixture(nn.Module):
+    """The MAdmixture (Modality Admixture) generic multimodal
+    model framework
+    
+    Arguments
+    ---------
+    modalities: list[Modality]
+        a list of modality specifications with encoder/decoder models
+
+    anchor_name: str
+        the name of the "anchor" modality, i.e. the one with which
+        all other modalities are aligned
+
+    latent_size: int
+        the size of the feature dimension of the latent space
+    """
+    def __init__(self, modalities, anchor_name, latent_size=32):
+        super().__init__()
+        self.modalities = {
+            modality.name: modality
+            for modality in modalities            
+        }
+        self.anchor_name = anchor_name
+        self.anchor = self.modalities[anchor_name]
+        self.latent_size = latent_size
+        self.aligned_modalities = {
+            key
+            for key, modality in self.modalities.items()
+            if modality.aligner
+        }
+        modalities = set(self.modalities.keys())
+        self.unaligned_modalities = modalities - self.aligned_modalities
+
+    def forward(self, inputs):
+        self.encode(inputs)
+
+    def encode(self, inputs, lengths):
+        """Encodes the inputs from each modality into the latent space
+        with the corresponding encoder
+        
+        Arguments
+        ---------
+        inputs: dict
+            A string -> tensor dictionary representing the model inputs
+
+        lengths: dict
+            A string -> tensor dictionary representing relative lengths
+            for variable-length inputs, where applicable
+
+            
+        Returns
+        -------
+        outputs: dict
+            A string->tensor dictionary with encoder outputs """
+
+        return {
+            key: self.modalities[key].encoder(
+                input,
+                lengths=lengths.get(key)
+            )
+            for key, input in inputs.items()
+        }
+
+    def latent(self, inputs, lengths):
+        """Computes the latent representations for each modality
+        by first running the encoders and then running the aligners,
+        if provided
+
+        Returns
+        -------
+        latents: dict
+            a key -> tensor dictionary of aligned latent representations
+        alignments: dict
+            a key -> tensor dictionary of alignment matrices, only for
+            modalities that support alignment
+        enc_out: dict
+            raw encoder ouputs for each modality        
+        """
+        enc_out = self.encode(inputs, lengths)
+        aligner_out = {
+            key: self.alignment(key, enc_out, lengths)
+            for key in inputs
+            if key in self.aligned_modalities
+        }
+        latents = {
+            key: latent
+            for key, (latent, _) in aligner_out.items()
+        } | {
+            key: enc_out[key]
+            for key in self.unaligned_modalities
+        }
+        alignments = {
+            key: alignment
+            for key, (alignment, _) in aligner_out.items()
+        }
+        return latents, alignments, enc_out
+
+    def alignment(self, key, enc_out, lengths):
+        """Computes the alignment for the specified modality
+
+        Aguments
+        --------
+        key: str
+            the modality key
+        enc_out
+            raw encoder outputs for all modalities
+        length: torch.Tensor
+            relative lengths
+            
+
+        Returns
+        -------
+        result: torch.Tensor
+            the aligned tensor
+        
+        alignment: torch.Tensor
+            the raw alignment matrix
+        """
+        if key in self.aligned_modalities:
+            result = self.modalities[key].aligner(key, enc_out, lengths)
+        else:
+            result = None, None
+        return result
+
+
+    def decode_single(self, latent, lengths):
+        """Decodes a single latent-space representation to all modalities. This
+        can be useful during inference (e.g. to reconstruct one modality from another
+        where applicable
+        
+        Arguments
+        ---------
+        latent: torch.Tensor
+            a single latent space batch
+        
+        length: torch.Tensor
+            the lengths of individual items"""
+        return {
+            key: modality.decoder(latent, lengths=lengths)
+            for key, modality in self.modalities.items()
+        }
+
+    def decode_multiple(self, latent, lengths):
+        """Decodes multiple latent-space representations using the decoder for
+        each corresponding modality. This can be useful during training
+        
+        Arguments
+        ---------
+        latent: dict
+            A str -> tensor dictionary representing a latent space representation
+            for multiple modalities
+        
+        
+        """
+        return {
+            key: self.modalities[key].decoder(
+                modality_latent,
+                length=lengths.get(key) if lengths is not None else None)
+            for key, modality_latent in latent
+        }
+
+
+class Modality(nn.Module):
+    """Represents a single modality within the MadMixture model
+    
+    Arguments
+    ---------
+    name: str
+        the modality identifier
+    
+    encoder: nn.Module
+        the encoder for the modality
+    
+    decoder: nn.Module
+        the decoder for the modality
+
+    aligner: nn.Module
+        an optional aligner module, aligning the outputs
+        to the latent representation
+
+    """
+    def __init__(self, name, encoder, decoder, aligner=None):
+        super().__init__()
+        self.name = name
+        self.encoder = encoder
+        self.decoder = decoder
+        self.aligner = aligner
+
+    def forward(self, input, length=None):
+        """Performs the encode operation (the default)
+        
+        Arguments
+        ---------
+        input: torch.Tensor
+            an input tensor for the modality
+        length: torch.Tensor
+            relative lengths
+        """
+        return self.encoder(input)
+
+
+class Aligner(nn.Module):
+    """A base class for auxiliary modules that align encoded outputs in
+    a given modality to the latent space"""
+    def forward(self, key, enc_out, length):
+        raise NotImplementedError()
+
+
+
+class AttentionalAligner(nn.Module):
+    """An aligner that uses an attention mechanism to align the raw encoder
+    outputs to the target latent space
+    
+    Arguments
+    ---------
+    key: str
+        the modality key
+    enc_out: torch.Tensor
+        raw encoded states
+    length: torch.Tensor
+        raw lengths
+    kernel_size: int
+        the kernel size 
+    scale: float
+        the scaling factor
+    """
+
+    def __init__(self, dim, nhead=1, dropout=0., scale=None, kernel_size=None):
+        super().__init__()
+        self.attn = MultiheadAttention(
+            nhead=nhead,
+            d_model=dim,
+            dropout=dropout
+        )
+        self.scale = scale
+        if scale is None or scale - 1 < 0.01:
+            self.scale = 1
+            self.compressor = nn.Identity()
+        elif scale > 1.:
+            stride = ceil(scale)
+            if kernel_size is None:
+                kernel_size = stride
+            padding = (kernel_size - stride) // 2
+            self.compressor = ConvTranspose1d(
+                in_channels=dim,
+                out_channels=dim,
+                stride=stride,
+                kernel_size=kernel_size,
+                padding=padding
+            )
+        else:
+            stride = floor(1 / scale)
+            if kernel_size is None:
+                kernel_size = scale
+            self.compressor = Conv1d(
+                in_channels=dim,
+                out_channels=dim,
+                stride=stride,
+                kernel_size=kernel_size,
+                padding=padding
+            )
+
+    def get_queries(self, enc_out):
+        """Upsamples or downsamples raw encoder outputs
+        before applying attention
+        
+        """
+        enc_out_scaled = self.compressor(enc_out)
+        new_length = floor(enc_out.size(1) * self.scale)
+        return enc_out_scaled[:, :new_length, :]
+
+    def forward(self, key, enc_out, lengths):
+        """Computes alignments using an attention module
+
+        Arguments
+        ---------
+        key: str
+            the modality key
+        enc_out: torch.Tensor
+            raw encoded states
+        length: torch.Tensor
+            relative lengths
+        
+        """
+        mod_enc_out = enc_out[key]
+        mod_length = lengths[key]
+        queries = self.get_queries(mod_enc_out)
+        max_len = queries.size(1)
+        mask = length_to_mask(mod_length * max_len, max_len).unsqueeze(-1)
+        masked_queries = queries * mask
+        output, alignment = self.attn(masked_queries, mod_enc_out, mod_enc_out)
+        return output * mask, alignment
