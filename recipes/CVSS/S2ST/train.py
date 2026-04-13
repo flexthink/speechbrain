d80017225fe9f047464d731538cfc83ad08780f2
flexthink <flexthink@users.noreply.github.com>
2026-04-13 11:50:39 -0400

    Fixes

--- recipes/CVSS/S2ST/train.py
+++ recipes/CVSS/S2ST/train.py
@@ -480,8 +480,8 @@ def dataio_prepare(hparams):
         datasets[split] = sb.dataio.dataset.DynamicItemDataset.from_json(
             json_path=hparams[f"{split}_json"],
             replacements={
-                "src_data_folder": hparams["src_data_folder"],
-                "tgt_data_folder": hparams["tgt_data_folder"],
+                "src_data_root": hparams["src_data_folder"],
+                "tgt_data_root": hparams["tgt_data_folder"],
             },
             dynamic_items=[
                 src_audio_pipeline,
